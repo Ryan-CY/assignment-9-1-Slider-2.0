b2a74c97b70394a7b2f669c107ed8b2da3e50e64
Ryan CY <phillin820109@msn.com>
2022-08-28 04:29:53 +0800

    Initial Commit

---
+++ assignment 9-1 Slider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment 9-1 Slider
+//
+//  Created by Ryan Lin on 2022/8/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
